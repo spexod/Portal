2f7b5d8e5dab478269d3149f842273c57f05e705
chw3k5 <chw3k5@gmail.com>
2021-10-28 15:29:56 -0500

    init and update script changes

--- init.sh
+++ init.sh
@@ -3,13 +3,13 @@ clear
 echo "SpExServer Initialization Script"
 cd /opt/bitnami/ || return
 PROJECT=projects
+cd $PROJECT/ || exit && docker-compose down && cd ../
 rm $PROJECT -rf
 mkdir $PROJECT && chmod 755 $PROJECT && cd $PROJECT/ || exit
 git clone https://github.com/chw3k5/SpExServer .
 git checkout main
 chmod 744 init_repos.sh init.sh update_repos.sh update.sh
 ./init_repos.sh
-docker-compose down
 docker-compose build
 docker-compose up -d
 echo "completed: SpExServer Initialization Script"
