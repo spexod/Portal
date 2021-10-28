ad1a9acb5f34b2f651c3a7f3a11390a5c6b2c6f8
chw3k5 <chw3k5@gmail.com>
2021-10-28 17:06:17 -0500

    Added the sql_config.py file to the init scripts

--- init.sh
+++ init.sh
@@ -7,9 +7,9 @@ PROJECT=projects
 cd $PROJECT/ || return && docker-compose down && cd ../
 rm $PROJECT -rf
 mkdir $PROJECT && chmod 755 $PROJECT && cd $PROJECT/ || exit
-cp /home/bitnami/sql_config.py .
 git clone https://github.com/chw3k5/SpExServer .
 git checkout main
+cp /home/bitnami/sql_config.py .
 chmod 744 init_repos.sh init.sh update_repos.sh update.sh
 cp update.sh ~/.
 ./init_repos.sh
