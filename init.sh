1e6c1766bf6526e9d49d57540adbbb803af74833
chw3k5 <chw3k5@gmail.com>
2021-10-28 15:13:03 -0500

    Dockerfile and script fixes

--- init.sh
+++ init.sh
@@ -7,8 +7,9 @@ rm $PROJECT -rf
 mkdir $PROJECT && chmod 755 $PROJECT && cd $PROJECT/ || exit
 git clone https://github.com/chw3k5/SpExServer .
 git checkout main
+chmod 744 init_repos.sh init.sh update_repos.sh update.sh
 ./init_repos.sh
 docker-compose down
 docker-compose build
 docker-compose up -d
-echo "completed: SpExServer Update Script"
+echo "completed: SpExServer Initialization Script"
