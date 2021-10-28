9fdb8d6a3a974963d8770b325da75350d66f5c2f
chw3k5 <chw3k5@gmail.com>
2021-10-28 16:49:12 -0500

    Added the sql_config.py file to the init scripts

--- init_repos.sh
+++ init_repos.sh
@@ -6,7 +6,10 @@ git checkout main
 echo "Downloading SpExWebsite Repository"
 git clone https://github.com/isaacj96/SpExWebsite
 git checkout master
+cp sql_config.py SpExWebsite/SpExoDisks/mypysql/.
+
 echo "Downloading SpExoDisks Repository"
 git clone https://github.com/chw3k5/SpExoDisks
 git checkout main
+cp sql_config.py SpExWebsite/SpExoDisks/mypysql/.
 echo "completed: SpExServer initialization Script"
