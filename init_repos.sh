6941186ac5dda39635a82565bf15e094cee03425
Caleb Wheeler <chw3k5@gmail.com>
2022-06-12 10:39:57 -0500

    modularization for SpExWenSite and SpExoDisks

--- init_repos.sh
+++ init_repos.sh
@@ -4,35 +4,17 @@ echo "Downloading SpExo-FrontEnd Repository"
 git clone https://github.com/L-Key/SpExo-FrontEnd
 cd SpExo-FrontEnd
 git checkout down-data
-chmod -R 777 .
 cd ../
+chmod -R 777 SpExo-FrontEnd
 
 echo "Downloading SpExWebsite Repository"
 git clone https://github.com/isaacj96/SpExWebsite
 cd SpExWebsite
 git checkout down-data
-chmod -R 777 .
 cd ../
-cp sql_config.py SpExWebsite/.
 
-echo "Downloading SpExoDisks Repository"
 cd SpExWebsite
-git clone https://github.com/chw3k5/SpExoDisks
-cd SpExoDisks
-git checkout main
-chmod -R 777 .
-cd ../
-cd ../
-cp sql_config.py SpExWebsite/SpExoDisks/mypysql/.
-
-echo "Downloading autostar Repository"
-cd SpExWebsite
-cd SpExoDisks
-git clone https://github.com/chw3k5/autostar
-cd autostar
-git checkout main
-chmod -R 777 .
-cd ../
-cd ../
+cp ../sql_config.py .
+./init_repos.sh
 cd ../
 echo "completed: SpExServer repository initialization script."
