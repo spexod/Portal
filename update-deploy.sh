b107a6c3b963ce1d053a1e0cd4f83d5f47e13a96
chw3k5 <chw3k5@gmail.com>
2022-07-04 16:22:27 -0500

    Docker-compose command order change to minimize down-time.

--- update-deploy.sh
+++ update-deploy.sh
@@ -2,10 +2,10 @@
 clear
 echo "SpExServer Update Script"
 cd /opt/bitnami/projects/ || return
-docker-compose -f docker-compose-deploy.yml down
 git checkout main
 git pull origin main
 ./update_repos.sh
+# try to build new images before taking down the old ones
 docker-compose -f docker-compose-deploy.yml build
 docker-compose -f docker-compose-deploy.yml down
 docker-compose -f docker-compose-deploy.yml up -d
