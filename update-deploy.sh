1c344df5c81de5ff71f813f6d5c7c1734294afee
chw3k5 <chw3k5@gmail.com>
2022-07-04 16:19:47 -0500

    Docker-compose command order change to minimize down-time.

--- update-deploy.sh
+++ update-deploy.sh
@@ -6,5 +6,7 @@ docker-compose -f docker-compose-deploy.yml down
 git checkout main
 git pull origin main
 ./update_repos.sh
-docker-compose -f docker-compose-deploy.yml up --build -d
+docker-compose -f docker-compose-deploy.yml build
+docker-compose -f docker-compose-deploy.yml down
+docker-compose -f docker-compose-deploy.yml up -d
 echo "completed: SpExServer Update Script"
