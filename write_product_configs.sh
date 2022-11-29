912ae2008f6cef38aa42ecf6b88457f904fbe2fe
chw3k5 <chw3k5@gmail.com>
2022-11-29 17:54:22 -0600

    Upgrades to allow SpExServer to set config details to specify builds for testing versus production.

---
+++ write_product_configs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "export const useServer=true;" > ./SpExo-FrontEnd/front-app/src/api_config.js
+echo -e "USE_NEW_TABLES = False\nDEBUG = False" > ./SpExWebsite/SpSite/data_config.py
---
+++ write_test_configs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo -e "export const useServer=false;" > ./SpExo-FrontEnd/front-app/src/api_config.js
+echo -e "USE_NEW_TABLES = True\nDEBUG = True" > ./SpExWebsite/SpSite/data_config.py
