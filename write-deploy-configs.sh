fe2bd4a35a56cbef64a023948a3338f33de4e1c9
Caleb Wheeler <chw3k5@gmail.com>
2023-01-19 19:13:50 -0600

    Updates to correctly run an independent test for new spectra.
    Update happened when making upgrades for the JWST-MIRI

--- write-deploy-configs.sh
+++ write-deploy-configs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo -e "export const useServer=true;" > ./SpExo-FrontEnd/front-app/src/api_config.js
 echo -e "USE_NEW_TABLES = False\nDEBUG = False" > ./SpExWebsite/SpSite/data_config.py
+echo -e "USE_NEW_TABLES = False\nDEBUG = False" > ./SpExWebsite/djangoAPI/data_config.py
 echo -e "do_test_run=false\nreset_status=false\nnew_data_staged='see server'\nupdated_mysql='see server'" > ./SpExWebsite/SpExoDisks/ref/data_status.toml
