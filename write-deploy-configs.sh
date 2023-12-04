51da072e75864e925a88941f9428ead03b885864
chw3k5 <chw3k5@gmail.com>
2023-12-04 17:13:02 -0600

    NEXTJS deployment, multi repo commit for stable docker build.

--- write-deploy-configs.sh
+++ write-deploy-configs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo -e "export const useServer=true;" > ./SpExo-FrontEnd/front-app/src/api_config.js
+echo -e "API_BASE_SERVER=http://backend:8000/\nNEXT_PUBLIC_API_BASE_CLIENT=https://spexodisks.com/" > ./SpExo-FrontEnd/context/.env.production
 echo -e "USE_NEW_TABLES = False\nDEBUG = False" > ./SpExWebsite/SpSite/data_config.py
 echo -e "USE_NEW_TABLES = False\nDEBUG = False" > ./SpExWebsite/djangoAPI/data_config.py
 echo -e "do_test_run=false\nreset_status=false\nnew_data_staged='see server'\nupdated_mysql='see server'" > ./SpExWebsite/SpExoDisks/ref/data_status.toml
