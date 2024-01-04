f3ae220fea51d7ae5d96116ed6d8209b733be57a
chw3k5 <chw3k5@gmail.com>
2024-01-04 12:03:10 -0600

    Deployment configuration mistake

--- shell/frontend-buildcache.sh
+++ shell/frontend-buildcache.sh
@@ -6,6 +6,7 @@ docker compose up --build --detach backend nginx
 read -r -p "Build a local API (backend) and NGINX-server if completed, press any key to build the frontend..."
 # build the frontend on the local machine (we need the cache from this for the docker-build later)
 cd SpExo-FrontEnd || exit
+npm install
 npm run build
 cd ../ || exit
 read -r -p "Local Build for frontend completed (needed for fetch-cache), press any key to launch the test-website and continue..."
