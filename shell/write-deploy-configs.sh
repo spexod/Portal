a8b751141ee5b4cae72b87cffc56aed24d70fb23
chw3k5 <chw3k5@gmail.com>
2024-03-02 21:35:01 -0600

    https need for api

--- shell/write-deploy-configs.sh
+++ shell/write-deploy-configs.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo -e "API_BASE_SERVER=http://backend:8000/\nNEXT_PUBLIC_API_BASE_CLIENT=http://spexodisks.com/" > ./SpExo-FrontEnd/.env.production
+echo -e "API_BASE_SERVER=http://backend:8000/\nNEXT_PUBLIC_API_BASE_CLIENT=https://spexodisks.com/" > ./SpExo-FrontEnd/.env.production
